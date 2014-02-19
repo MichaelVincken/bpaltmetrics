aa0a11d6c137afe78058cac2e76c33ca823045ca
MichaelVincken <michaelvinckenbe@gmail.com>
2014-02-19 01:55:49 +0100

    Skip this Database.
    Heading at the end of the file.
    </table> closed.

--- insert_person2.php
+++ insert_person2.php
@@ -4,14 +4,14 @@ $page_title = 'Insert Person';
 include_once('menu.php');
 include_once('scrape.php');
 include_once('database.php');
-//get data from POST
+
+// Retrieve data from POST
 $firstname =  ucfirst(mysql_real_escape_string($_POST["firstname"]));
 $lastname =  ucfirst(mysql_real_escape_string($_POST["lastname"]));
-        
 $network_string =  mysql_real_escape_string($_POST["networks"]);
 $network_array = unserialize(urldecode($network_string));
 
-//We are done, there are no more networks.                
+// -- CASE 0 :: There are no more networks --              
 if (count($network_array) == 0) {
     ?>
     <form name="auto_form" action="insert_person_end.php" method="post">
@@ -23,49 +23,53 @@ if (count($network_array) == 0) {
     </script>            
     <?php
 } else {
-    //Getting data for the next network.
+    
+    // Retrieve data for the next network.
     $network_name = $network_array[0];
     $urls = call_user_func_array($network_name.'_scrape'.'::search_person',array($firstname,$lastname));
             
-    $column_array = retrieve_columns($network_name.'_person',$con);
-            
+    $column_array = retrieve_columns($network_name.'_person',$con);        
 }
-//Case when this person is already in the database.
+
+// -- CASE 1 :: This person is already into the database --
 if (already_in_database($firstname,$lastname,$con) && count($network_array) == count(retrieve_networks($con))) {
     echo "<h2> " . $firstname . " " . $lastname . " has already been inserted in the database </h2>";
     echo "You can however continue if you know a new network will be added for this person. </br>";
-?>
-    <a href = "insert_person1.php"> Go back and insert a new person </a>
-<?php    
-}  
-    
-if ($urls == null) {        
-?>
-<h2>The person you're looking for has not been found in the <?php echo ucwords($network_name)?> network.</h2>
-<h3>Supply the right URL yourself</h3>
-<form name="confirm" action="insert_person2_URLNotFound.php" method="post">
-    url: <input type="url" name="url"/>
-    <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
-    <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
-    <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
-    <input type="submit" value="confirm">
-</form>
-<h3>Continue with the next network</h3>
-<form name="confirm" action="insert_person2.php" method="post">
-    <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
-    <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
-                    
-    <?php
-    array_shift($network_array); 
     ?>
+    <a href = "insert_person1.php"> Go back and insert a new person </a>
+    <?php
+}  
+
+// -- CASE 2 :: This person has not been found in this network --
+if ($urls == null) {        
+    ?>
+    <h2>The person you're looking for has not been found in the <?php echo ucwords($network_name)?> network.</h2>
+    <h3>Supply the right URL yourself</h3>
+    <form name="confirm" action="insert_person2_URLNotFound.php" method="post">
+        url: <input type="url" name="url"/>
+        <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
+        <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
+        <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
+        <input type="submit" value="confirm">
+    </form>
+    <h3>Continue with the next network</h3>
+    <form name="confirm" action="insert_person2.php" method="post">
+        <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
+        <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
                     
-    <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
-    <input type="submit" value="confirm">
-</form>
-<?php
+        <?php
+        array_shift($network_array); 
+        ?>
+                    
+        <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
+        <input type="submit" value="confirm">
+    </form>
+    <?php
 } else {
-    ?>        
-    <h2><?php echo ucwords($network_name)?> Netwerk: Select the person you're looking for.</h2>
+    
+    // -- CASE 3 :: Selecting the person to be inserted in the database --
+    ?>
+    <h2><?php echo ucwords($network_name)?> Network: Select the person you're looking for.</h2>
     <table>
         <thead>
             <?php                
@@ -95,45 +99,41 @@ if ($urls == null) {
                     }
                     
                     
-                ?>
-                <td>
-                    <form name="confirm" action="insert_person3.php" method="post">
-                        <?php 
-                        $resultstring = urlencode(serialize($result));
-                        ?>
-                        <input type="hidden" value="<?php echo $resultstring ?>" name="result" />
-                        <input type="hidden" value="<?php echo $url ?>" name="url" />
-                        <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
-                        <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
-                        <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
-                        <input type="submit" value="confirm">
-                    </form>
-
-                    <?php
-                
-                echo "</td>"; 
-                echo "</tr>";
+                    ?>
+                    <td>
+                        <form name="confirm" action="insert_person3.php" method="post">
+                            <?php 
+                            $resultstring = urlencode(serialize($result));
+                            ?>
+                            <input type="hidden" value="<?php echo $resultstring ?>" name="result" />
+                            <input type="hidden" value="<?php echo $url ?>" name="url" />
+                            <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
+                            <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
+                            <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
+                            <input type="submit" value="confirm">
+                        </form>
+                        <?php
+                        echo "</td>"; 
+                        echo "</tr>";
+                    }
                 }
-        }
-        echo "</tbody>";
-        ?>
-    
-        <h3>Skip this database.</h3>
+                ?>
+            </tbody>
+        </table>
+            <?php
+        } ?>
+        <h3>Or skip this database if you don't want to insert this person in the <?php echo ucwords($network_name)?> Network.</h3>
         <form name="confirm" action="insert_person2.php" method="post">
             <input type="hidden" value="<?php echo $firstname ?>" name="firstname" />
             <input type="hidden" value="<?php echo $lastname ?>" name="lastname" />
-                    
+    
             <?php
             array_shift($network_array); 
             ?>
-                    
+    
             <input type="hidden" value="<?php echo urlencode(serialize($network_array)) ?>" name="networks" />
             <input type="submit" value="confirm">
         </form>
-            
-                </br>
-        
         <?php
-}
-
-include('footer.php');
+            include('footer.php');
+            ?>
