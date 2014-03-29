b6a6393e2d939fa476288d7ad61a283bba768a56
MichaelVincken <michaelvinckenbe@gmail.com>
2014-03-29 20:43:22 +0100

    Index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,4 +2,10 @@
 $page_title = "Welcome";
 include('/home/thesis-std/menu.php');
 ?>
-Welcome
+<strong>Hello.</strong>
+Thank you for your interest in our application that we have developed for our bachelor thesis. Before we get started, there are a few things to know:
+
+This is our very first release, so you'll get encounter quality issues. The goal of this release to help us improve through feedback.
+
+We are looking for researchers that want to evaluate this application. First get to know the application. Afterwards you can enter the <a href="https://docs.google.com/forms/d/1ei4-gOfS3E0vX5AYge9Ts0O1eyddDkJ0jydYEGrZZTA/viewform">survey</a>. It will only take 5-10 minutes of your time.
+
