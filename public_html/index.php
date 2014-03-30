78ee001d81bdff9a4de38a0d5d17a259d39a60cc
MichaelVincken <michaelvinckenbe@gmail.com>
2014-03-30 21:17:46 +0200

    Index

--- public_html/index.php
+++ public_html/index.php
@@ -2,12 +2,29 @@
 $page_title = "Welcome";
 include('/home/thesis-std/menu.php');
 ?>
-<strong>Hello.</strong>
-Thank you for your interest in our application that we have developed for our bachelor thesis. Before we get started, there are a few things to know:
+<table>
+ <thead>
+  <tr>
+     <th colspan = "2"><h1>Welcome to our webapplication.</h1></th>
+  </tr>
+ </thead>
 
-This is our very first release, so you'll get encounter quality issues. The goal of this release to help us improve through feedback.
+ <tbody>
+  <tr rowspan = "2">
+     <td><a href = "manual.pdf" target="_blank"><img src="manual.png" alt="Manual" height="80" width="80"></td>
+     <td><strong>Quick Introduction to the Application</strong></br>Learn essential information about using the application .</td>
+  </tr>
 
-We are looking for researchers that want to evaluate this application. First get to know the application. Afterwards you can enter the <a href="https://docs.google.com/forms/d/1ei4-gOfS3E0vX5AYge9Ts0O1eyddDkJ0jydYEGrZZTA/viewform">survey</a>. It will only take 5-10 minutes of your time.
-</br>
-<a href = "manual.pdf" target="_blank"><img src="manual.png" alt="Manual" height="205" width="280"></a>
+  <tr>
+     <td><a href="https://docs.google.com/forms/d/1ei4-gOfS3E0vX5AYge9Ts0O1eyddDkJ0jydYEGrZZTA/viewform" target="_blank"><img src="survey.png" alt="Manual" height="10" width="10"></td>
+       <td><strong>Give us feedback by filling out a survey</strong></br>First get to know the application. Afterwards you can enter the survey. It will only take 5-10 minutes of your time.</td>
+  </tr>
+
+  <tr>
+     <td><a href="mailto:stvm.bachelorproef@gmail.com" target="_blank"><img src="contactUs.png" alt="Manual" height="10" width="10"></td>
+       <td><strong>Contact us</strong></br>If you want to know more about us or the application, you can always mail us: <a href="mailto:stvm.bachelorproef@gmail.com">bachelorproef@gmail.com</a></td>
+  </tr>
+
+ </tbody>
+</table>
 
